ddcbb245eb174085ce4eebfd9b97a466f0063597
Sardar-Taha <tahaabb1122@gmail.com>
2023-01-10 20:59:52 +0500

    so

--- index.js
+++ index.js
@@ -30,3 +30,14 @@ $cell.find(".js-collapser").click(function () {
   $thisCell.removeClass("is-expanded").addClass("is-collapsed");
   $cell.not($thisCell).removeClass("is-inactive");
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+};
